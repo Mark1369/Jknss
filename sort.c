1135b7a3376217e6f9630674222ec0f798fa29e9
Mark1369 <71013170+Mark1369@users.noreply.github.com>
2026-03-09 09:01:30 +0100

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[] = {5, 3, 6, 7, 1, 4};
+    int n = 6;
+
+    printf("P2 - Zadanie Jenkins\n");
+    
+    printf("Tablica przed sortowaniem: ");
+    for(int i=0;i<n;i++){
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    // proste sortowanie (bubble sort)
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(numbers[i] > numbers[j]){
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+
+    printf("Tablica po sortowaniu: ");
+    for(int i=0;i<n;i++){
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
